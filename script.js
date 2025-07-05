23c1ab4ea3f4add48c41147c2a1b3af34b137fcb
yashG <yrg.creation.ai@gmail.com>
2025-07-05 20:06:56 +0530

    landing page added

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-gsap.to(".nav-li", {
-    yPercent: -100,
-    opacity: 0,
+
+gsap.to(".nav-li a", {
+    // yPercent: -100,
+    // opacity: 0,
     duration: 0.2,
-    stagger: 0.05,           // little cascade, optional
+        color: "#bc6c25",
+    // stagger: 0.05,           // little cascade, optional
     scrollTrigger: {
       trigger: "header",     // element whose position we watch
       start: "top top",      // when header’s top meets viewport top
       end: "top -10%",       // 10 % further up the page
       scrub: true,
     //   markers: true   
-    }   
+    }
 });
 
+gsap.to(".menu i", {
+    color: "#bc6c25",
+    duration: 1.5,
+    delay: 1,
+    scrollTrigger: {
+      trigger: "header",     // element whose position we watch
+      start: "top top",      // when header’s top meets viewport top
+      end: "top -10%",       // 10 % further up the page
+      scrub: true,
+    //   markers: true   
+    }
+});
